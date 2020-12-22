08ce27ceaef032747dc8bbfd3215b7d9f322165e
Pansun <chkehua@gmail.com>
2020-12-22 15:49:40 +0700

    添加项目文件。

---
+++ FileLock/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace FileLock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEncryptFile = new System.Windows.Forms.Button();
+            this.btnDecryptFile = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "choose file:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "password:";
+            // 
+            // btnEncryptFile
+            // 
+            this.btnEncryptFile.Location = new System.Drawing.Point(247, 188);
+            this.btnEncryptFile.Name = "btnEncryptFile";
+            this.btnEncryptFile.Size = new System.Drawing.Size(75, 23);
+            this.btnEncryptFile.TabIndex = 2;
+            this.btnEncryptFile.Text = "Encrypt file";
+            this.btnEncryptFile.UseVisualStyleBackColor = true;
+            this.btnEncryptFile.Click += new System.EventHandler(this.btnEncryptFile_Click);
+            // 
+            // btnDecryptFile
+            // 
+            this.btnDecryptFile.Location = new System.Drawing.Point(380, 188);
+            this.btnDecryptFile.Name = "btnDecryptFile";
+            this.btnDecryptFile.Size = new System.Drawing.Size(75, 23);
+            this.btnDecryptFile.TabIndex = 3;
+            this.btnDecryptFile.Text = "Decrypt file";
+            this.btnDecryptFile.UseVisualStyleBackColor = true;
+            this.btnDecryptFile.Click += new System.EventHandler(this.btnDecryptFile_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(111, 20);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(185, 21);
+            this.txtFileName.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(111, 73);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(185, 21);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(316, 17);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(38, 23);
+            this.btnSelectFile.TabIndex = 6;
+            this.btnSelectFile.Text = "...";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 246);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnDecryptFile);
+            this.Controls.Add(this.btnEncryptFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "DES-Encrypt/Decrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEncryptFile;
+        private System.Windows.Forms.Button btnDecryptFile;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnSelectFile;
+    }
+}
+
---
+++ FileLock/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Security.Cryptography;
+using System.IO;
+
+namespace FileLock
+{    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtFileName.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void btnEncryptFile_Click(object sender, EventArgs e)
+        {
+            string inFile = txtFileName.Text;
+            string outFile = inFile + ".txt";
+            string password = txtPassword.Text;
+            FileLock.DESFileClass.EncryptFile(inFile, outFile, password);//Encrypt file
+            //delete old file
+            File.Delete(inFile);
+            txtFileName.Text = string.Empty;
+            MessageBox.Show("Encrypt successfully");
+        }
+
+        private void btnDecryptFile_Click(object sender, EventArgs e)
+        {
+            string inFile = txtFileName.Text;
+            string outFile = inFile.Substring(0, inFile.Length - 4);
+            string password = txtPassword.Text;
+            FileLock.DESFileClass.DecryptFile(inFile, outFile, password);//Decrypt file
+            //delete old file
+            File.Delete(inFile);
+            txtFileName.Text = string.Empty;
+            MessageBox.Show("Decrypt successfully");
+        }
+
+        
+    }
+}
